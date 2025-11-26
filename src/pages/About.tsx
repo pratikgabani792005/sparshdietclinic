1070cb7249248968e62abdebd6be6fa8a72901aa
Pratik <pratikgabani79@gmail.com>
2025-11-26 18:26:53 +0530

    favicon added and meta description and title added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -269,7 +269,7 @@ export function About() {
                 className="w-full h-full object-contain bg-white"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-stone-900/80 via-stone-900/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-6">
-                <p className="text-white" style={{ fontWeight: 600 }}>
+                <p className="text-white" style={{ fontWeight: 600 , color : '#ffffff'}}>
                   {cert.title}
                 </p>
               </div>
